45e71caddc2a80c1ce3dc55e251ceabb7f866ba7
kunalgaurav <kunalgaurav1420@gmail.com>
2023-07-17 15:18:12 +0530

    jsx & props in react

--- src/App.js
+++ src/App.js
@@ -2,21 +2,47 @@ import './App.css';
 
 
 
-function Mycomponent(){
-  return <div>
+function Mycomponent({name, nameforclass, income=10000000 }){
+  return (
+    <>
+  
+    <div className={nameforclass}>
+  
+   {name} : {income}
+  
+    </div>
+    
+    </>
+  )
 
-    kunal gaurav
-  </div>;
-}
+};
 
 function App() {
   return (
 
+    
     <div id="adas" className="custom" >
-      hello <Mycomponent/> guys
+      hello kunal 
+      <Mycomponent name={"abhishek"}   income={"500"} nameforclass="div1"/> 
+
+      <Mycomponent name={"kunal gaurav"}   income={45333} nameforclass="div2"/> 
+
+      <Mycomponent name={"priti"}    nameforclass="div3"/>  
+
+      <Mycomponent name={"chahal"}  income={4344334} nameforclass="div4"/>
 
       <button >click me!</button>
     </div>
+
+    
+    
+
+
+
+
+
+  
+   
    
     
     
