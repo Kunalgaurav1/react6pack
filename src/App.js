a364072705a815ccc663558173027a12861c8ea7
kunalgaurav <kunalgaurav1420@gmail.com>
2023-07-18 15:09:30 +0530

    components & styling in react

--- src/App.js
+++ src/App.js
@@ -1,50 +1,35 @@
 import './App.css';
+import Header from './Components/Header.js';
+import {Footer, Footer2} from './Components/Footer.js';
 
 
 
-function Mycomponent({name, nameforclass, income=10000000 }){
-  return (
-    <>
-  
-    <div className={nameforclass}>
-  
-   {name} : {income}
-  
-    </div>
-    
-    </>
-  )
 
-};
 
 function App() {
   return (
-
-    
+    <>
     <div id="adas" className="custom" >
       hello kunal 
-      <Mycomponent name={"abhishek"}   income={"500"} nameforclass="div1"/> 
-
-      <Mycomponent name={"kunal gaurav"}   income={45333} nameforclass="div2"/> 
-
-      <Mycomponent name={"priti"}    nameforclass="div3"/>  
-
-      <Mycomponent name={"chahal"}  income={4344334} nameforclass="div4"/>
-
-      <button >click me!</button>
     </div>
 
-    
-    
+    <Header/>
+
+    <Footer/>
+
+    <Footer2/>
 
 
 
 
 
-  
-   
-   
-    
+
+
+
+
+
+
+    </>
     
   );
 }
---
+++ src/Components/Header.js
@@ -0,0 +1,32 @@
+
+//rfc
+// import React from 'react'
+
+// export default function Header() {
+//   return (
+//     <div>Header</div>
+//   )
+// }
+
+//rfce
+// import React from 'react'
+
+// function Header() {
+//   return (
+//     <div>Header</div>
+//   )
+// }
+
+// export default Header
+
+//rafce
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>Header</div>
+    
+  )
+}
+
+export default Header;
